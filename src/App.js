09a06a0bcdd60f18bcec5309a2982df642d824e7
PhamHoangDung <phamhoangdung3968@gmail.com>
2025-11-06 13:08:48 +0700

    test API

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 const handleLogin = async (e) => {
   e.preventDefault();
   try {
-    const res = await axios.post('http://localhost:3000/auth/login', {
+    const res = await axios.post('http://localhost:3001/auth/login', {
       username,
       password
     });
